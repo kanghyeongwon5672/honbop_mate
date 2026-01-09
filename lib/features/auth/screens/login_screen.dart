698638c21464f9baba89091306f45b419e6e214f
sleep0107 <suji9078@gmail.com>
2026-01-09 18:08:19 +0900

    아이콘 테스트

---
+++ lib/features/auth/screens/login_screen.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+import '../services/google_auth_service.dart';
+/*
+class LoginPage extends StatelessWidget {
+  const LoginPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () async {
+            // 구글 로그인 시도
+            await GoogleAuthService().signInWithGoogle();
+            // 성공하면 StreamBuilder가 자동으로 화면을 'home page'로 바꿔줍니다.
+          },
+          child: const Text('Google Login'),
+        ),
+      ),
+    );
+  }
+}
+*/
